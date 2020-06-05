265fede0bccd4724cd56a34e2f7f6246a90bcb4f
laloinsane <calisto.michel@gmail.com>
2020-06-05 12:45:51 -0400

    Nivel 2 funcional

--- scripts/Nivel2.gd
+++ scripts/Nivel2.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 export(PackedScene) var Objeto
-const objects_file = "res://data/animals.json"
+var objects_file
 var json
 var audio = AudioStreamPlayer.new()
 var ogg = AudioStreamOGGVorbis.new()
@@ -13,8 +13,10 @@ var time_left = 2
 var eleccion_correcta1 = false
 var eleccion_correcta2 = false
 var eleccion_correcta3 = false
+var eleccion_correcta4 = false
 
 func _ready():
+	objects_file = "res://data/"+Global.categorie+".json"
 	# HUD
 	$HUD.update_level(Global.nivels_level)
 	$HUD.update_score(Global.nivels_score)
@@ -22,34 +24,37 @@ func _ready():
 	# Panels
 	if Global.nivels_level == 1:
 		update_indicaciones("[center]ESCUCHA CON ATENCION [img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-		var panels_x = 515 - (125 * (2-1))
+		var panels_x = 555 - (125 * (2-1))
 		$Panels.rect_position.x = panels_x
 		$Panels/Margin1.visible = true
 		$Panels/Margin2.visible = true
 	if Global.nivels_level == 2:
 		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-		var panels_x = 515 - (125 * (2-1))
-		$Panels.rect_position.x = panels_x
-		# Panels
-		$Panels/Margin1.visible = true
-		$Panels/Margin2.visible = true
-	if Global.nivels_level == 3:
-		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-		var panels_x = 515 - (125 * (3-1))
+		var panels_x = 555 - (125 * (3-1))
 		$Panels.rect_position.x = panels_x
 		# Panels
 		$Panels/Margin1.visible = true
 		$Panels/Margin2.visible = true
 		$Panels/Margin3.visible = true
-	if Global.nivels_level == 4:
+	if Global.nivels_level == 3:
 		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-		var panels_x = 515 - (125 * (4-1))
+		var panels_x = 555 - (125 * (4-1))
 		$Panels.rect_position.x = panels_x
 		# Panels
 		$Panels/Margin1.visible = true
 		$Panels/Margin2.visible = true
 		$Panels/Margin3.visible = true
 		$Panels/Margin4.visible = true
+	if Global.nivels_level == 4:
+		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
+		var panels_x = 555 - (125 * (5-1))
+		$Panels.rect_position.x = panels_x
+		# Panels
+		$Panels/Margin1.visible = true
+		$Panels/Margin2.visible = true
+		$Panels/Margin3.visible = true
+		$Panels/Margin4.visible = true
+		$Panels/Margin5.visible = true
 	# Transition
 	$Transition.fadeOut()
 	yield($Transition/AnimationPlayer, "animation_finished")
@@ -61,17 +66,17 @@ func _ready():
 	if Global.nivels_level == 1:
 		set_sounds(2)
 		_on_Escuchar_pressed()
-		set_options(json, 0)
+		set_options(json, 1)
 	if Global.nivels_level == 2:
-		set_sounds(2)
+		set_sounds(3)
 		_on_Escuchar_pressed()
 		set_options(json, 1)
 	if Global.nivels_level == 3:
-		set_sounds(3)
-		_on_Escuchar_pressed()
-		set_options(json, 0)
-	if Global.nivels_level == 4:
 		set_sounds(4)
+		_on_Escuchar_pressed()
+		set_options(json, 1)
+	if Global.nivels_level == 4:
+		set_sounds(5)
 		_on_Escuchar_pressed()
 		set_options(json, 0)
 
@@ -108,6 +113,10 @@ func _process(delta):
 					if $Options.get_child(count).liberado == true:
 						$Options.get_child(count).set_obj_slot($Panels/Margin2/Panel2.get_global_rect().position.x, $Panels/Margin2/Panel2.get_global_rect().position.y, 2)
 						$Panels/Margin2/Panel2.set_slot_item(true)
+				elif $Panels/Margin3/Panel3.get_global_rect().has_point(cursor_pos) and $Panels/Margin3/Panel3.contain_slot_item == false:
+					if $Options.get_child(count).liberado == true:
+						$Options.get_child(count).set_obj_slot($Panels/Margin3/Panel3.get_global_rect().position.x, $Panels/Margin3/Panel3.get_global_rect().position.y, 3)
+						$Panels/Margin3/Panel3.set_slot_item(true)
 				elif $Options.get_child(count).liberado == true:
 					$Options.get_child(count).reset_start_position()
 			count += 1
@@ -127,6 +136,10 @@ func _process(delta):
 					if $Options.get_child(count).liberado == true:
 						$Options.get_child(count).set_obj_slot($Panels/Margin3/Panel3.get_global_rect().position.x, $Panels/Margin3/Panel3.get_global_rect().position.y, 3)
 						$Panels/Margin3/Panel3.set_slot_item(true)
+				elif $Panels/Margin4/Panel4.get_global_rect().has_point(cursor_pos) and $Panels/Margin4/Panel4.contain_slot_item == false:
+					if $Options.get_child(count).liberado == true:
+						$Options.get_child(count).set_obj_slot($Panels/Margin4/Panel4.get_global_rect().position.x, $Panels/Margin4/Panel4.get_global_rect().position.y, 4)
+						$Panels/Margin4/Panel4.set_slot_item(true)
 				elif $Options.get_child(count).liberado == true:
 					$Options.get_child(count).reset_start_position()
 			count += 1
@@ -150,6 +163,10 @@ func _process(delta):
 					if $Options.get_child(count).liberado == true:
 						$Options.get_child(count).set_obj_slot($Panels/Margin4/Panel4.get_global_rect().position.x, $Panels/Margin4/Panel4.get_global_rect().position.y, 4)
 						$Panels/Margin4/Panel4.set_slot_item(true)
+				elif $Panels/Margin5/Panel5.get_global_rect().has_point(cursor_pos) and $Panels/Margin5/Panel5.contain_slot_item == false:
+					if $Options.get_child(count).liberado == true:
+						$Options.get_child(count).set_obj_slot($Panels/Margin5/Panel5.get_global_rect().position.x, $Panels/Margin5/Panel5.get_global_rect().position.y, 5)
+						$Panels/Margin5/Panel5.set_slot_item(true)
 				elif $Options.get_child(count).liberado == true:
 					$Options.get_child(count).reset_start_position()
 			count += 1
@@ -263,23 +280,7 @@ func _on_Escuchar_pressed():
 		$Panels.visible = true
 		$Options.visible = true
 		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiobasic.png")
-	if Global.nivels_level == 2 and seleccionados != 2:
-		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiopress.png")
-		$Margin/Escuchar.disabled = true
-		for i in $Sounds.get_children():
-			if seleccionados != 2:
-				ogg = load(i.get_sound())
-				ogg.loop = false
-				audio.stream = ogg
-				audio.play()
-				yield(audio, "finished")
-			else:
-				break
-		$Margin/Escuchar.disabled = false
-		$Panels.visible = true
-		$Options.visible = true
-		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiobasic.png")
-	if Global.nivels_level == 3 and seleccionados != 3:
+	if Global.nivels_level == 2 and seleccionados != 3:
 		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiopress.png")
 		$Margin/Escuchar.disabled = true
 		for i in $Sounds.get_children():
@@ -295,11 +296,27 @@ func _on_Escuchar_pressed():
 		$Panels.visible = true
 		$Options.visible = true
 		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiobasic.png")
-	if Global.nivels_level == 4 and seleccionados != 4:
+	if Global.nivels_level == 3 and seleccionados != 4:
 		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiopress.png")
 		$Margin/Escuchar.disabled = true
 		for i in $Sounds.get_children():
 			if seleccionados != 4:
+				ogg = load(i.get_sound())
+				ogg.loop = false
+				audio.stream = ogg
+				audio.play()
+				yield(audio, "finished")
+			else:
+				break
+		$Margin/Escuchar.disabled = false
+		$Panels.visible = true
+		$Options.visible = true
+		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiobasic.png")
+	if Global.nivels_level == 4 and seleccionados != 5:
+		$Margin/Escuchar.texture_normal = load("res://assets/buttons/nivels/audiopress.png")
+		$Margin/Escuchar.disabled = true
+		for i in $Sounds.get_children():
+			if seleccionados != 5:
 				ogg = load(i.get_sound())
 				ogg.loop = false
 				audio.stream = ogg
@@ -355,6 +372,18 @@ func _is_order_and_code(x):
 				incorrect(x)
 		elif seleccionados == 1:
 			seleccionados += 1
+			var existe = false
+			for i in $Sounds.get_children():
+				if x.order_slot != 0 and x.order_slot == i.order_audio:
+					if i.code == x.code:
+						existe = true
+			if existe == true:
+				eleccion_correcta2 = true
+				correct(x)
+			else:
+				incorrect(x)
+		elif seleccionados == 2:
+			seleccionados += 1
 			intentos_level += 1
 			audio.stop()
 			var existe = false
@@ -392,6 +421,18 @@ func _is_order_and_code(x):
 			else:
 				incorrect(x)
 		elif seleccionados == 2:
+			seleccionados += 1
+			var existe = false
+			for i in $Sounds.get_children():
+				if x.order_slot != 0 and x.order_slot == i.order_audio:
+					if i.code == x.code:
+						existe = true
+			if existe == true:
+				eleccion_correcta3 = true
+				correct(x)
+			else:
+				incorrect(x)
+		elif seleccionados == 3:
 			seleccionados += 1
 			intentos_level += 1
 			audio.stop()
@@ -443,6 +484,18 @@ func _is_order_and_code(x):
 				incorrect(x)
 		elif seleccionados == 3:
 			seleccionados += 1
+			var existe = false
+			for i in $Sounds.get_children():
+				if x.order_slot != 0 and x.order_slot == i.order_audio:
+					if i.code == x.code:
+						existe = true
+			if existe == true:
+				eleccion_correcta4 = true
+				correct(x)
+			else:
+				incorrect(x)
+		elif seleccionados == 4:
+			seleccionados += 1
 			intentos_level += 1
 			audio.stop()
 			var existe = false
@@ -473,7 +526,10 @@ func correct(x):
 			x.set_status("res://assets/icons/correct.png")
 			x.set_sound_victory()
 		elif seleccionados == 2:
-			if eleccion_correcta1 == true:
+			x.set_status("res://assets/icons/correct.png")
+			x.set_sound_victory()
+		elif seleccionados == 3:
+			if eleccion_correcta1 == true and eleccion_correcta2 == true:
 				score_level += 1
 				score_total += 1
 			$HUD.update_score(score_total)
@@ -488,7 +544,10 @@ func correct(x):
 			x.set_status("res://assets/icons/correct.png")
 			x.set_sound_victory()
 		elif seleccionados == 3:
-			if eleccion_correcta1 == true and eleccion_correcta2 == true:
+			x.set_status("res://assets/icons/correct.png")
+			x.set_sound_victory()
+		elif seleccionados == 4:
+			if eleccion_correcta1 == true and eleccion_correcta2 == true and eleccion_correcta3 == true:
 				score_level += 1
 				score_total += 1
 			$HUD.update_score(score_total)
@@ -506,7 +565,10 @@ func correct(x):
 			x.set_status("res://assets/icons/correct.png")
 			x.set_sound_victory()
 		elif seleccionados == 4:
-			if eleccion_correcta1 == true and eleccion_correcta2 == true and eleccion_correcta3 == true:
+			x.set_status("res://assets/icons/correct.png")
+			x.set_sound_victory()
+		elif seleccionados == 5:
+			if eleccion_correcta1 == true and eleccion_correcta2 == true and eleccion_correcta3 == true and eleccion_correcta4 == true:
 				score_level += 1
 				score_total += 1
 			$HUD.update_score(score_total)
@@ -530,6 +592,9 @@ func incorrect(x):
 		elif seleccionados == 2:
 			x.set_status("res://assets/icons/incorrect.png")
 			x.set_sound_lose()
+		elif seleccionados == 3:
+			x.set_status("res://assets/icons/incorrect.png")
+			x.set_sound_lose()
 			$Timer.start()
 	if Global.nivels_level == 3:
 		if seleccionados == 1:
@@ -539,6 +604,9 @@ func incorrect(x):
 			x.set_status("res://assets/icons/incorrect.png")
 			x.set_sound_lose()
 		elif seleccionados == 3:
+			x.set_status("res://assets/icons/incorrect.png")
+			x.set_sound_lose()
+		elif seleccionados == 4:
 			x.set_status("res://assets/icons/incorrect.png")
 			x.set_sound_lose()
 			$Timer.start()
@@ -553,6 +621,9 @@ func incorrect(x):
 			x.set_status("res://assets/icons/incorrect.png")
 			x.set_sound_lose()
 		elif seleccionados == 4:
+			x.set_status("res://assets/icons/incorrect.png")
+			x.set_sound_lose()
+		elif seleccionados == 5:
 			x.set_status("res://assets/icons/incorrect.png")
 			x.set_sound_lose()
 			$Timer.start()
@@ -586,37 +657,41 @@ func next():
 	eleccion_correcta1 = false
 	eleccion_correcta2 = false
 	eleccion_correcta3 = false
-	
+	eleccion_correcta4 = false
+		
 	if Global.nivels_level == 1:
 		$Panels/Margin1/Panel1.set_slot_item(false)
 		$Panels/Margin2/Panel2.set_slot_item(false)
 	if Global.nivels_level == 2:
 		$Panels/Margin1/Panel1.set_slot_item(false)
 		$Panels/Margin2/Panel2.set_slot_item(false)
+		$Panels/Margin3/Panel3.set_slot_item(false)
 	if Global.nivels_level == 3:
 		$Panels/Margin1/Panel1.set_slot_item(false)
 		$Panels/Margin2/Panel2.set_slot_item(false)
 		$Panels/Margin3/Panel3.set_slot_item(false)
+		$Panels/Margin4/Panel4.set_slot_item(false)
 	if Global.nivels_level == 4:
 		$Panels/Margin1/Panel1.set_slot_item(false)
 		$Panels/Margin2/Panel2.set_slot_item(false)
 		$Panels/Margin3/Panel3.set_slot_item(false)
 		$Panels/Margin4/Panel4.set_slot_item(false)
+		$Panels/Margin5/Panel5.set_slot_item(false)
 	if intentos_level < 2:
 		if Global.nivels_level == 1:
 			set_sounds(2)
 			_on_Escuchar_pressed()
-			set_options(json, 0)
+			set_options(json, 1)
 		if Global.nivels_level == 2:
-			set_sounds(2)
+			set_sounds(3)
 			_on_Escuchar_pressed()
 			set_options(json, 1)
 		if Global.nivels_level == 3:
-			set_sounds(3)
-			_on_Escuchar_pressed()
-			set_options(json, 0)
-		if Global.nivels_level == 4:
 			set_sounds(4)
+			_on_Escuchar_pressed()
+			set_options(json, 1)
+		if Global.nivels_level == 4:
+			set_sounds(5)
 			_on_Escuchar_pressed()
 			set_options(json, 0)
 	else:
@@ -636,29 +711,32 @@ func next():
 func next_level(s,t,l):
 	if l == 2:
 		# Panels
-		var panels_x = 515 - (125 * (2-1))
-		$Panels.rect_position.x = panels_x
-		$Panels/Margin1.visible = true
-		$Panels/Margin2.visible = true
-		# Display
-		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-	if l == 3:
-		# Panels
-		var panels_x = 515 - (125 * (3-1))
+		var panels_x = 555 - (125 * (3-1))
 		$Panels.rect_position.x = panels_x
 		$Panels/Margin1.visible = true
 		$Panels/Margin2.visible = true
 		$Panels/Margin3.visible = true
 		# Display
 		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
-	if l == 4:
+	if l == 3:
 		# Panels
-		var panels_x = 515 - (125 * (4-1))
+		var panels_x = 555 - (125 * (4-1))
 		$Panels.rect_position.x = panels_x
 		$Panels/Margin1.visible = true
 		$Panels/Margin2.visible = true
 		$Panels/Margin3.visible = true
 		$Panels/Margin4.visible = true
+		# Display
+		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
+	if l == 4:
+		# Panels
+		var panels_x = 555 - (125 * (5-1))
+		$Panels.rect_position.x = panels_x
+		$Panels/Margin1.visible = true
+		$Panels/Margin2.visible = true
+		$Panels/Margin3.visible = true
+		$Panels/Margin4.visible = true
+		$Panels/Margin5.visible = true
 		# Display
 		update_indicaciones("[center][img=40x40]res://assets/emojis/emoji_u1f449_1f3fb.png[/img] [img=40x40]res://assets/emojis/emoji_u1f442_1f3fb.png[/img]\n\"[color=#FFD948]ARRASTRA[/color] LAS IMAGENES SEGUN LO ESCUCHADO\"[/center]")
 	$Margin/Escuchar.visible = true
@@ -677,20 +755,21 @@ func next_level(s,t,l):
 	eleccion_correcta1 = false
 	eleccion_correcta2 = false
 	eleccion_correcta3 = false
+	eleccion_correcta4 = false
 	# Functions
 	reset_containers()
 	$HUD.update_level(Global.nivels_level)
 	json = read_json()
 	set_objects(json)
 	if Global.nivels_level == 2:
-		set_sounds(2)
+		set_sounds(3)
 		_on_Escuchar_pressed()
 		set_options(json, 1)
 	if Global.nivels_level == 3:
-		set_sounds(3)
-		_on_Escuchar_pressed()
-		set_options(json, 0)
-	if Global.nivels_level == 4:
 		set_sounds(4)
+		_on_Escuchar_pressed()
+		set_options(json, 1)
+	if Global.nivels_level == 4:
+		set_sounds(5)
 		_on_Escuchar_pressed()
 		set_options(json, 0)
